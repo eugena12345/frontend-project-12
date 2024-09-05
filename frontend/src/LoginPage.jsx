54cd6c4fc4627af4f9665dd0f6f0a7f2657be685
Eugena <eugena.h12345@gmail.com>
2024-09-05 22:25:00 +0300

    add formik to login page

--- frontend/src/LoginPage.jsx
+++ frontend/src/LoginPage.jsx
@@ -1,9 +1,38 @@
-const PageTwo = () => {
-    return ((
-        <div>
-            LoginPageLoginPageLoginPageLoginPageLoginPageLoginPageLoginPageLoginPage
-        </div>
-    ))
+import { useFormik } from 'formik';
+
+
+const LoginPage = () => {
+    const formik = useFormik({
+        initialValues: {
+            name: '',
+            email: '',
+        },
+        onSubmit: (values) => {
+            console.log(JSON.stringify(values, null, 2));
+        },
+    });
+    return (
+        <form onSubmit={formik.handleSubmit}>
+            <label htmlFor="name">UserName</label>
+            <input
+                id="name"
+                name="name"
+                type="name"
+                onChange={formik.handleChange}
+                value={formik.values.name}
+            />
+            <label htmlFor="email">Email Address</label>
+            <input
+                id="email"
+                name="email"
+                type="email"
+                onChange={formik.handleChange}
+                value={formik.values.email}
+            />
+
+            <button type="submit">Submit</button>
+        </form>
+    );
 }
 
-export default PageTwo;
+export default LoginPage;
