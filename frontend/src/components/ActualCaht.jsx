4fc4fde7013c0efeb82cb0264caa364407d04691
Eugena <eugena.h12345@gmail.com>
2024-09-17 23:17:14 +0300

    add filter messages by channel

--- frontend/src/components/ActualCaht.jsx
+++ frontend/src/components/ActualCaht.jsx
@@ -11,17 +11,23 @@ const ActualChat = () => {
     const messages = useSelector(messagesSelectors.selectAll);
     console.log('messages!!!!!!!!!!!!!! /n', messages);
 
+    const currentMessages = currentChannel ?
+        messages.filter((message) => message.channelId === currentChannel.id)
+        : [];
+
+
+
     return (
         <div className='d-flex flex-column h-100'>
             {currentChannel &&
                 <>
                     <div className="bg-light mb-4 p-3 shadow-sm small">
                         <p className="m-0">#{currentChannel.name}</p>
-                        <span>{messages.length} сообщений</span>
+                        <span>{currentMessages.length} сообщений</span>
                     </div>
                     <div className='chat-messages'>
                         <div className="overflow-auto px-5">
-                            {messages.map((message) => {
+                            {currentMessages.map((message) => {
                                 return (
                                     <p><b>{message.username}:</b> {message.body}</p>
                                 )
