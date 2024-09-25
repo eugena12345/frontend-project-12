7cf151c9d8f4909309dc723309ae0498589ff028
Eugena <eugena.h12345@gmail.com>
2024-09-25 22:41:10 +0300

    add navigation to PageOne by click on header

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -2,10 +2,10 @@ import Nav from 'react-bootstrap/Nav';
 import { actions as autorizedActions } from '../slices/auorizeSlice.js';
 import { useDispatch } from 'react-redux';
 import Button from 'react-bootstrap/Button';
-
-
+import { useNavigate } from 'react-router-dom';
 
 const Header = ({ setUser }) => {
+    const navigate = useNavigate();
     const dispatch = useDispatch();
     let userToken = localStorage.getItem('token');
 
@@ -13,9 +13,13 @@ const Header = ({ setUser }) => {
         setUser(null);
         dispatch(autorizedActions.logout())
     };
+
+    const goToPageOne = () => {
+        navigate('/', { replace: false });
+    }
     return (
         <Nav className='shadow-sm navbar navbar-expand-lg navbar-light bg-primary px-4'>
-            <h2 className="h-2 text-white">Hexlet chat</h2>
+            <h2 className="h-2 text-white" onClick={goToPageOne}>Hexlet chat</h2>
             <div className="p-2 ms-auto">
                 {userToken && <Button className='btn btn-outline-light' onClick={logout}>Выйти</Button>}
             </div>
