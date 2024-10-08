6c26d6c085c93e54f72474fd4e360072939d777a
Eugena <eugena.h12345@gmail.com>
2024-10-08 23:33:00 +0300

    fix some linter errors

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css';
+import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import PageOne from './components/PageOne';
 import LoginPage from './components/LoginPage';
@@ -6,18 +7,15 @@ import NotFoundPage from './components/NotFoundPage';
 import RegistrationPage from './components/RegistrationPage';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="*" element={<NotFoundPage />} />
-        <Route path="/" element={<PageOne />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/signup" element={<RegistrationPage />} />
-      </Routes>
-    </BrowserRouter>
-
-  );
-}
+const App = () => (
+  <BrowserRouter>
+    <Routes>
+      <Route path="*" element={<NotFoundPage />} />
+      <Route path="/" element={<PageOne />} />
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/signup" element={<RegistrationPage />} />
+    </Routes>
+  </BrowserRouter>
+);
 
 export default App;
