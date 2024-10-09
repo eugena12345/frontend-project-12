2e9215d02aee156d1f46cf640aefec8606a207a8
Eugena <eugena.h12345@gmail.com>
2024-10-09 17:00:49 +0300

    fix some linter errors

--- frontend/src/components/ChannelNameModal.jsx
+++ frontend/src/components/ChannelNameModal.jsx
@@ -1,3 +1,5 @@
+import React from 'react';
+
 import Form from 'react-bootstrap/Form';
 import Modal from 'react-bootstrap/Modal';
 import Button from 'react-bootstrap/Button';
@@ -6,12 +8,16 @@ import { useTranslation } from 'react-i18next';
 import { useFormik } from 'formik';
 import * as yup from 'yup';
 
-const ChannelNameModal = ({ show, handleClose, channelsNameColl, modalContent }) => {
-  const { t, i18n } = useTranslation();
-
-  console.log(modalContent);
+const ChannelNameModal = (
+  {
+    show,
+    handleClose,
+    channelsNameColl,
+    modalContent,
+  },
+) => {
+  const { t } = useTranslation();
   const oldChannelName = modalContent.oldChannelName;
-  console.log(oldChannelName);
   const formik = useFormik({
     enableReinitialize: true,
     initialValues: {
@@ -37,36 +43,35 @@ const ChannelNameModal = ({ show, handleClose, channelsNameColl, modalContent })
 
   return (
     <Modal show={show} onHide={handleClose}>
-            <Modal.Header closeButton>
-                <Modal.Title>{modalContent.text}</Modal.Title>
-            </Modal.Header>
-            <Modal.Body>
-                <Form onSubmit={formik.handleSubmit}>
-                    <Form.Group className="mb-3 focus-ring" controlId="exampleForm.ControlInput1">
-
-                        <Form.Label>{t('channelName')}</Form.Label>
-                        <Form.Control
-                          name="channelName"
-                            type="text"
-                            autoFocus
-                            value={formik.values.channelName}
-                            onChange={formik.handleChange}
-                            onBlur={formik.handleBlur}
-                        />
-                        {formik.submitCount > 0 && formik.errors.channelName && (
-                            <p className='text-danger small'>{formik.errors.channelName}</p>
-                        )}
-                    </Form.Group>
-                    <div className='d-flex justify-content-end'>
-                        <Button variant="secondary m-1" onClick={handleClose}>
-                            {t('button.cancel')}
-                        </Button>
-                        <Button variant="primary" type='submit'>
-                            {t('button.create')}
-                        </Button>
-                    </div>
-                </Form>
-            </Modal.Body>
+      <Modal.Header closeButton>
+        <Modal.Title>{modalContent.text}</Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <Form onSubmit={formik.handleSubmit}>
+          <Form.Group className="mb-3 focus-ring" controlId="exampleForm.ControlInput1">
+            <Form.Label>{t('channelName')}</Form.Label>
+            <Form.Control
+              name="channelName"
+              type="text"
+              autoFocus
+              value={formik.values.channelName}
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+            />
+            {formik.submitCount > 0 && formik.errors.channelName && (
+            <p className="text-danger small">{formik.errors.channelName}</p>
+            )}
+          </Form.Group>
+          <div className="d-flex justify-content-end">
+            <Button variant="secondary m-1" onClick={handleClose}>
+              {t('button.cancel')}
+            </Button>
+            <Button variant="primary" type="submit">
+              {t('button.create')}
+            </Button>
+          </div>
+        </Form>
+      </Modal.Body>
     </Modal>
   );
 };
