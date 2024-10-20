c9acdf51d548b360945025b40ab72ab40e215617
Eugena <eugena.h12345@gmail.com>
2024-10-20 21:55:53 +0300

    delete linter comment

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -93,13 +93,11 @@ const LoginPage = () => {
                   {t('logIn')}
                 </Button>
               </Form>
-
               {/* eslint-disable-next-line jsx-a11y/click-events-have-key-events,
                jsx-a11y/no-static-element-interactions */}
               <div className="registration" onClick={goToRegistration}>
                 {t('noAccount')}
                 {' '}
-                {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
                 <p className="text-primary"><u>{t('registrationHere')}</u></p>
               </div>
             </div>
