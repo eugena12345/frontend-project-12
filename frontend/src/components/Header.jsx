29ecad2a7b64afc41611b0b895a493df52a2c888
Eugena <eugena.h12345@gmail.com>
2024-11-04 20:25:42 +0300

    add Link

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,8 +1,7 @@
-/* eslint-disable react/prop-types */
 import React from 'react';
 import Nav from 'react-bootstrap/Nav';
 import Button from 'react-bootstrap/Button';
-import { useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { useTranslation } from 'react-i18next';
 import store from '../store';
 
@@ -16,14 +15,11 @@ const Header = ({ onLogoutClick }) => {
     navigate('/login', { replace: false });
   };
 
-  const goToMainPage = () => {
-    navigate('/', { replace: false });
-  };
   return (
     <Nav className="shadow-sm navbar navbar-expand-lg navbar-light bg-primary px-4">
-      {/* eslint-disable-next-line jsx-a11y/click-events-have-key-events,
-       jsx-a11y/no-noninteractive-element-interactions */}
-      <h2 className="h-2 text-white" onClick={goToMainPage}>{t('hexletChat')}</h2>
+      <Link to="/">
+        <h2 className="h-2 text-white">{t('hexletChat')}</h2>
+      </Link>
       <div className="p-2 ms-auto">
         {userToken && <Button className="btn btn-outline-light" onClick={logout}>{t('logOut')}</Button>}
       </div>
