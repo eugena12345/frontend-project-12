44bd98b2fc8d75c19eebb60538b242b1497380a1
Eugena <eugena.h12345@gmail.com>
2024-11-06 21:35:57 +0300

    return useEffect to Private route

--- frontend/src/components/PrivateRoute.jsx
+++ frontend/src/components/PrivateRoute.jsx
@@ -1,14 +1,17 @@
+import { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import store from '../store';
 
 const PrivateRoute = ({ children }) => {
   const navigate = useNavigate();
   const userToken = store.getState().user.token;
-  if (!userToken) {
-    navigate('/login', { replace: false });
-    return null;
-  }
-  return children;
+  useEffect(() => {
+    if (!userToken) {
+      navigate('/login', { replace: false });
+    }
+  }, [userToken, navigate]);
+
+  return userToken && children;
 };
 
 export default PrivateRoute;
